e25b3b043deefdd5e666c82fc5a85f4464666788
cheesemori <2274159818@qq.com>
2026-01-30 11:35:31 +0000

    Updated views.py to use the new index.html template

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # 2. 使用 render 函数
+    # render(request对象, 模板路径, 上下文数据字典)
+    return render(request, 'rango/index.html', context=context_dict)
 
