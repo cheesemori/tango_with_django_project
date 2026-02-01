305ee504693db0488b67ee06ae413f27ddcc3b16
cheesemori <2274159818@qq.com>
2026-02-01 16:27:37 +0000

    Finish Chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -7,11 +7,10 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-    # 1. 定义 Python 类别的页面数据
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url': 'http://docs.python.org/3/tutorial/',
-         'views': 114}, # 为了看起来更真实，我加了些随机views，测试脚本只查是否存在
+         'views': 114},
         {'title': 'How to Think like a Computer Scientist',
          'url': 'http://www.greenteapress.com/thinkpython/',
          'views': 53},
@@ -20,7 +19,6 @@ def populate():
          'views': 20}
     ]
 
-    # 2. 定义 Django 类别的页面数据
     django_pages = [
         {'title': 'Official Django Tutorial',
          'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
@@ -33,7 +31,7 @@ def populate():
          'views': 1258}
     ]
 
-    # 3. 定义 Other Frameworks 类别的页面数据
+
     other_pages = [
         {'title': 'Bottle',
          'url': 'http://bottlepy.org/docs/dev/',
@@ -43,21 +41,17 @@ def populate():
          'views': 3}
     ]
 
-    # 4. 组装类别数据 (注意：这里设置了 views 和 likes，测试脚本会检查这些数值！)
     cats = {
         'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
         'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
         'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
     }
 
-    # 5. 循环创建数据
     for cat, cat_data in cats.items():
-        # add_cat 函数在下面定义
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'], views=p['views'])
 
-    # 打印结果
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
@@ -76,7 +70,6 @@ def add_cat(name, views=0, likes=0):
     c.save()
     return c
 
-# 执行代码
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
